a6d639bd7119e7d0b02abb60af4b3d9c966842de
Hashh_dg63@top-academy.ru <Hashh_dg63@top-academy.ru>
2024-10-01 22:37:59 +0300

    handle user input.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,7 +10,24 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            string[] choices = { "камень", "ножницы", "бумага" };
 
+            Random random = new Random();
+
+            string playAgain = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Выберите: камень, ножницы или бумага:");
+                string userChoice = Console.ReadLine().ToLower();
+
+                if (Array.IndexOf(choices, userChoice) < 0)
+                {
+                    Console.WriteLine("Ошибка! Пожалуйста, выберите камень, ножницы или бумагу");
+                    continue;
+                }
+            }
+            while (playAgain == "y");
         }
     }
 }
